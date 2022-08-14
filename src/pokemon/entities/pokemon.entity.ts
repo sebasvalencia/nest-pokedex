1b23d1762e1589d6a30a673eae4edbc6dd8a6f2e
Sebastian Valencia <sebastian.valencia@yuxiglobal.com>
2022-08-13 20:35:50 -0500

    feat: crear modelo y schema para el pokemon

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,3 +1,26 @@
 //representacion entidad en la BD
 
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  //id: string; MOngo me lo da
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+//Cuando se crea la BD
+//muestra las definiciones
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
