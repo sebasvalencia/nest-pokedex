2358f225511a3736a41e48c44d11a156f9ab7c4d
sebasvalencia <sebastian.valencian@gmail.com>
2022-08-16 20:16:41 -0500

    feat: create axios adapter and use it on the seed service

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokemonResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({}); //delete * from pokemons
 
-    const { data } = await this.axios.get<PokemonResponse>(
+    const data = await this.http.get<PokemonResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
     const pokemonToInsert: { name: string; no: number }[] = [];
-   
+
     data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
